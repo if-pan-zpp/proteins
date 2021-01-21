363cb5eb32863b8709b7f0b922fee59c4b6e9dd1
Kuba Boguta <kuba.boguta2@gmail.com>
2021-01-21 18:51:44 +0100

    Rough implementation of Simulation.run(), redesigned Potential class

--- pt/src/main.cpp
+++ pt/src/main.cpp
@@ -1,13 +1,17 @@
 #include <fstream>
 #include "pdb/ir/parser.h"
 #include "state/def.h"
+#include "def/types.h"
 #include "conf/constants.h"
 #include <eigen3/Eigen/Core>
 using namespace std;
 
 class Config {
 public:
-    Config(int argc, char **argv);
+    Config(int argc, char **argv) {
+	// Here there will be a lot of parsing configs, checking them,
+	// printing to the user, prompting the user...
+    }
 
     // Angle potential:
     bool angle_pot = true;
@@ -16,93 +20,166 @@ public:
 
     // Other configs:
     string out_filename;
+    int max_steps = 10;
+    def::Scalar verlet_list_max_eps = 10.0;
     // ...
 };
 
+/*
+ * Class that stores the state of the environment. 
+ * Examples: temperature, movement of walls, current step.
+ */
 class State {
 public:
-    
-    void init(const Config &config);
+    State(const Config &config) {
+	max_steps = config.max_steps;
+    }
+    bool is_running() {
+	return cur_step < max_steps;
+    }
+    void take_step() {
+	cur_step++;
+    }
+    int max_steps = 0;
+    int cur_step = 0;
+    def::Scalar temperature = 0.15;
 };
 
-class VerList {
-public:
-    
-    void init(const Config &config);
-};
 
 /* Class for storing pseudo-atoms.
  */
 class PAtoms {
 public:
-    void init(const Config &config);
+    PAtoms(const Config &config) {
+	n = 1;
+    }
     int n;
     array<Vec3DArray, DER_ORDER + 1> der; // positions' derivatives.
     
 };
 
+class VerList {
+public:
+    VerList(const Config &_config, const PAtoms &_p_atoms) {
+	eps_upper_bound = _config.verlet_list_max_eps;
+    }
+
+    using VerIt = vector<pair<int,int>>::const_iterator;
+    pair<VerIt, VerIt> get_verlet_list(def::Scalar eps) const {
+	return {list.begin(), list.end()};
+    }
+
+    void take_step() {
+    }
+
+    void register_eps(def::Scalar req_eps) {
+	assert (req_eps < eps_upper_bound);
+	biggest_req_eps = max(biggest_req_eps, req_eps);
+    }
+
+private:
+    def::Scalar eps_upper_bound;
+    def::Scalar biggest_req_eps = 0.;
+    vector<pair<int, int>> list;
+};
+
+
 class Statistics {
 public:
-    void init(const Config &config);
+    Statistics(const Config &_config,
+	       const State &_state,
+	       const PAtoms &_p_atoms)
+	: state(_state), p_atoms(_p_atoms) {};
+
+    void take_step() {
+	cout << "We're in step " << state.cur_step << endl;
+	
+    }
+private:
+    const State &state;
+    const PAtoms &p_atoms;
 };
 
 class Integrator {
 public:
-    void init(const Config &config, const PAtoms &p_atoms);
-    void step(PAtoms &p_atoms);
+    Integrator(const Config &_config,
+	       const State &_state,
+	       const PAtoms &_p_atoms) : state(_state) {}
+    void step(PAtoms &p_atoms, const Vec3DArray &forces) {
+	def::Scalar temp = state.temperature;
+	// Implement predictor-corrector here.
+    }
 private:
+    const State &state;
 };
 
 // POTENTIALS
 
 class Potential {
 public:
-    virtual Vec3DArray calculate_forces(
-	const State &state,
-	const PAtoms &p_atoms,
-	const VerList &verlet_list,
-	Statistics &stats) = 0;
+    Potential(const Config &_config,
+	      const State &_state,
+	      const PAtoms &_p_atoms) :
+	state(_state), p_atoms(_p_atoms) {}
 
-    virtual bool is_enabled(const Config &config) const = 0;
-    virtual void init(const Config &config) {};
+    virtual void init_and_register(VerList &verlet_list) {}
+    virtual void init_step() {}
+    virtual Vec3DArray calculate_forces(const VerList &verlet_list) = 0;
+    virtual void finish_step(Statistics &statistics) {}
+    virtual bool is_enabled() const = 0;
+
+protected:
+    const State &state;
+    const PAtoms &p_atoms;
 };
 
 class BondAnglePotential : public Potential {
 public:
-    Vec3DArray calculate_forces(
-	const State &state,
-	const PAtoms &p_atoms,
-	const VerList &verlet_list,
-	Statistics &stats) override { return Vec3DArray(); }
-    bool is_enabled(const Config &config) const override; 
-    void init(const Config &config) override {}
-};
+    BondAnglePotential(const Config &_config,
+		       const State &_state,
+		       const PAtoms &_p_atoms) :
+	Potential(_config, _state, _p_atoms) {
 
-bool BondAnglePotential::is_enabled(const Config &config) const {
-    return config.angle_pot;
-}
+	enabled = _config.angle_pot;
+    }
+
+    Vec3DArray calculate_forces(const VerList &verlet_list) override {
+	return Vec3DArray::Zero(p_atoms.n, 3);
+    }
+    bool is_enabled() const override {
+	return enabled;
+    }
+private:
+    bool enabled = false;
+};
 
 class DihAnglePotential : public Potential {
 public:
-    Vec3DArray calculate_forces(
-	const State &state,
-	const PAtoms &p_atoms,
-	const VerList &verlet_list,
-	Statistics &stats) override { return Vec3DArray(); }
-    bool is_enabled(const Config &config) const override; 
-    void init(const Config &config) override {}
+    DihAnglePotential(const Config &_config,
+		      const State &_state,
+		      const PAtoms &_p_atoms) :
+	Potential(_config, _state, _p_atoms) {
+
+	enabled = _config.dih_angle_pot;
+    }
+
+    void init_and_register(VerList &verlet_list) override {
+	// Right now, if this potential is enabled, this will fail,
+	// because maximum eps is 10.0.
+	verlet_list.register_eps(15.0);
+    }
+    Vec3DArray calculate_forces(const VerList &verlet_list) override {
+	return Vec3DArray::Zero(p_atoms.n, 3);
+    }
+    bool is_enabled() const override {
+	return enabled;
+    }
+private:
+    bool enabled = false;
 };
 
-bool DihAnglePotential::is_enabled(const Config &config) const {
-    return config.dih_angle_pot;
-}
+// END OF POTENTIALS
 
-vector<unique_ptr<Potential>> all_supported_potentials() {
-    vector<unique_ptr<Potential>> all;
-    all.emplace_back(make_unique<BondAnglePotential>());
-    all.emplace_back(make_unique<DihAnglePotential>());
-    return all;
-}
 
 
 /*
@@ -120,21 +197,58 @@ private:
     PAtoms p_atoms; 
     vector<shared_ptr<Potential>> potentials;
     Integrator integrator;
+    Statistics statistics;
+
+    Vec3DArray forces;
+
+    void take_step();
+    vector<unique_ptr<Potential>> all_supported_potentials(const Config &config) const;
 };
 
-Simulation::Simulation(const Config &config) {
-    for (auto &pot_ptr: all_supported_potentials()) {
-	if (pot_ptr -> is_enabled(config)) potentials.emplace_back(move(pot_ptr));
-    }
-
-    for (auto &pot_ptr : potentials) pot_ptr -> init(config);
-
-    cout << "Number of potentials: " << potentials.size() << endl;
+vector<unique_ptr<Potential>> Simulation::all_supported_potentials(const Config &config) const {
+    vector<unique_ptr<Potential>> all;
+    all.emplace_back(make_unique<BondAnglePotential>(config, state, p_atoms));
+    all.emplace_back(make_unique<DihAnglePotential>(config, state, p_atoms));
+    return all;
 }
 
-void Simulation::run() {}
+Simulation::Simulation(const Config &config) :
+    state(config),
+    p_atoms(config),
+    verlet_list(config, p_atoms),
+    integrator(config, state, p_atoms),
+    statistics(config, state, p_atoms)
+    {
+    
+    for (auto &pot_ptr: all_supported_potentials(config)) {
+	if (pot_ptr -> is_enabled()) potentials.emplace_back(move(pot_ptr));
+    }
 
-Config::Config(int argc, char **argv) {}
+    for (auto &pot_ptr : potentials) pot_ptr -> init_and_register(verlet_list);
+
+    cout << "Number of enabled potentials: " << potentials.size() << endl;
+}
+
+void Simulation::take_step() {
+    state.take_step();
+    verlet_list.take_step();
+
+    forces = Vec3DArray::Zero(p_atoms.n, 3);
+    for (auto &pot_ptr : potentials) {
+	pot_ptr -> init_step();
+	forces += pot_ptr -> calculate_forces(verlet_list);
+	pot_ptr -> finish_step(statistics);
+    }
+
+    integrator.step(p_atoms, forces);
+    statistics.take_step();
+}
+
+void Simulation::run() {
+    while (state.is_running()) {
+	take_step();
+    }
+}
 
 int main(int argc, char **argv) {
     Config config(argc, argv);
